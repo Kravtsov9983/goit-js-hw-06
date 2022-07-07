72e55365ff6d7a8332037698b8f0a69ff0b58bfc
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-07 20:12:19 +0300

    task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -14,20 +14,17 @@ const images = [
 ];
 
 
-const box = document.querySelector("ul")
-const article =
-  images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-]; 
-box.insertAdjacentHTML("beforeend", article);
+const ul = document.querySelector("ul")
+const article = `<li>
+  src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt = 'White and Black Long Fur Cat'
+</li>
+<li>
+  src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt = 'Orange and White Koi Fish Near Yellow Koi Fish'
+</li>
+<li>
+  src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt = 'Group of Horses Running'
+</li>`
+ul.insertAdjacentHTML("beforeend", article);
