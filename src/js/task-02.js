b5f85375493bd621010bfbd97e22960cb781655e
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-07 16:36:48 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector("ul")
+const li = document.createElement("li")
+li.textContent = "Potatoes"
+li.className = "item"
+ul.append(li)
+
+const article =
+  `<li class="item">Mushrooms</li>
+  <li class="item">Garlic</li>
+  <li class="item">Tomatos</li>
+  <li class="item">Herbs</li>
+  <li class="item">Condiments</li>`; 
+ul.insertAdjacentHTML("beforeend", article);
