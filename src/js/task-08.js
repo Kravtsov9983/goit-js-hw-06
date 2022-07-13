469acdd41e3c4c4b1855680f133abe22531e7ba0
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-13 12:06:53 +0300

    Поправив task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,23 +1,60 @@
-const form = document.querySelector(`.login-form`);
-form.addEventListener(`submit`, onFormSubmit);
+const form = document.querySelector(".login-form");
 
-function onFormSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-
-
-    formData.forEach((value, name) => {
-        if (value === "" ) {
+function hendleSubmit(event) {
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email;
+    const password = formElements.password;
+    
+if (email.value === "" || password.value === ""  ) {
         alert('Будь-ласка заповніть поля!')
         form.reset();
         return
+}
+    
+    event.preventDefault();
+    const formData = {
+        email: email.value,
+        password: password.value
+
+        
     }
-        console.log(`onFormSubmit -> name`, name)
-        console.log(`onFormSubmit -> value`, value)
-        form.reset();
-        return
-    })
+    console.log(formData)
+    form.reset()
+
+    
+}
+form.addEventListener("submit", hendleSubmit)
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector(`.login-form`);
+// form.addEventListener(`submit`, onFormSubmit);
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.currentTarget);
+
+
+//     formData.forEach((value, name) => {
+//         if (value === "" ) {
+//         alert('Будь-ласка заповніть поля!')
+//         form.reset();
+//         return
+//     }
+//         console.log(`onFormSubmit -> name`, name)
+//         console.log(`onFormSubmit -> value`, value)
+//         form.reset();
+//         return
+//     })
 
 
 
@@ -34,7 +71,7 @@ function onFormSubmit(event) {
     // }
 
     // console.log(dataForm)
-}
+// }
 
  // if (email === "" || password === "") {
     //     alert('Будь-ласка заповніть поля!')
